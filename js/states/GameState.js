6f288bfb418c7afd6b1f0c1c41c848bd26122754
CameronPennington <cameron.pennington@gmail.com>
2020-05-31 11:59:33 -0400

    Implemented states and added GAME OVER screen

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -1,21 +1,4 @@
 var GameState = {
-	//initiate some game-level settings
-	init: function () {
-		//scaling options
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
-	},
-	//load the game assets before the game starts
-	preload: function () {
-		this.load.image("backyard", "assets/images/backyard.png");
-		this.load.image("apple", "assets/images/apple.png");
-		this.load.image("candy", "assets/images/candy.png");
-		this.load.image("rotate", "assets/images/rotate.png");
-		this.load.image("toy", "assets/images/rubber_duck.png");
-		this.load.image("arrow", "assets/images/arrow.png");
-		this.load.spritesheet("pet", "assets/images/pet.png", 97, 83, 5, 1, 1);
-	},
 	//executed after everything is loaded
 	create: function () {
 		this.background = this.game.add.sprite(0, 0, "backyard");
@@ -156,6 +139,7 @@ var GameState = {
 		}
 	},
 	gameOver: function () {
-		this.game.state.restart();
+		//the start method fires the init method of the state being called
+		this.state.start("HomeState", true, false, "GAME OVER");
 	},
 };
